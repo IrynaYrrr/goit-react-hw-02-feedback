b7b32daa5db2a96a68335623bca67364005eca7c
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-07-23 20:16:01 +0300

    complete step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,55 @@
-import Feedback from './feedback/Feedback';
+import React, { Component } from "react";
 
-export const App = () => {
-  return (
-    <div
-      style={{
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+
+  handleGood = () => {
+    this.setState((prevState) => {
+      return {
+        good: prevState.good + 1,
+      }
+    })
+  }
+
+  handleNeutral = () => {
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1,
+      }
+    })
+  }
+
+  handleBad = () => {
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1
+      }
+    })
+  }
+
+  render() {
+
+    return (
+      <div style={{
         height: '40vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      Please leave feedback:
-      <Feedback />
-    </div>
-  );
-};
+      }}>
+
+        <span className='title-feedback'>Please leave feedback:</span>
+        <button onClick={this.handleGood} type="button" className='feedback-btn good-btn'>good: {this.state.good}</button>
+        <button onClick={this.handleNeutral} type="button" className='feedback-btn neutral-btn'>neutral: {this.state.neutral}</button>
+        <button onClick={this.handleBad} type="button" className='feedback-btn bad-btn'>bad: {this.state.bad}</button>
+      </div>
+    );
+  }
+}
